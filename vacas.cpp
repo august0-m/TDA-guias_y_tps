be8677423988edb9d764abdffd65adcded436bad
august0-m <152826406+august0-m@users.noreply.github.com>
2024-09-25 03:30:48 -0300

    1 adentro ojota

--- vacas.cpp
+++ vacas.cpp
@@ -3,15 +3,34 @@
 #include <algorithm>
 
 using namespace std;
-int T; N, C;
-vector<int> p;
+int T, N, C;
+vector<vector<int>> p;
 
+
+void funcionarda(vector<vector<int>> v){
+            
+}
+
+//leo los inputs y los ordeno en vectores
 int main (){
 
-    cin T >> N >> C;
+    cin >> T;
+    p.resize(T);
 
-    p.resize(N, 0);
-    for (int i=0; i<N; i++) cin >> p[i];
-    sort(p.begin(),p.end());
+    for (int i=0; i<T; i++){
+    cin >> N >> C ;
+     p[i].resize(N);
+     for (int j=0;j<N;j++){
+         cin >> p[i][j];
+     }
+    sort(p[i].begin(),p[i].end());
+    }
+    //for (int i = 0; i < T; i++) {
+    //    cout << "Caso " << i + 1 << ": ";
+    //    for (int j = 0; j < p[i].size(); j++) {
+    //        cout << p[i][j] << " ";  // Imprime cada elemento del vector
+    //    }
+    //    cout << endl;  // Nueva línea después de cada caso
+    //}
     return 0;
 }
